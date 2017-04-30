dae66c7ee1921d3d9fdf2049a96950f2e26052ea
炫 <yinshengfeng@foxmail.com>
2017-04-30 19:28:49 +0800

    控制浏览器窗口大小
    控制浏览器窗口大小

---
+++ test.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+from selenium import webdriver
+
+driver=webdriver.Chrome()
+driver.get("http://m.mail.10086.cn")
+"""参数数字为像素点"""
+print("设置浏览器宽480、高800显示")
+driver.set_window_size(480,800)
